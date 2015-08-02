634a47d3635d4c9e0e62ffe6c7499d329f023b94
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-02 16:56:06 +0200

    another solution ported, also haskell

---
+++ 008/haskell/solution.hs
@@ -0,0 +1,26 @@
+import           Control.Monad
+
+p :: Int
+p = 5
+
+-- Split an integer into its digits
+digits :: Integral x => x -> [x]
+digits 0 = []
+digits x = digits (x `div` 10) ++ [x `mod` 10]
+
+-- Generate the list of all possible sets of q adjacent elements from a list
+setsOf :: Int -> [a] -> [[a]]
+setsOf q l | length l < q = [[]]
+setsOf q list = (take q list):(setsOf q $ tail list)
+
+solve :: Integer -> Integer
+solve nr = (maximum . (map product) . (setsOf p) . digits) nr
+
+-- IO
+rd :: String -> Integer
+rd x  = read x :: Integer
+
+main :: IO ()
+main = do
+    ds <- replicateM 20 getLine
+    putStrLn $ (show . solve . rd . concat) ds
