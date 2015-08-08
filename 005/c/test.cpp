8db8766a6271a21be90e11694c64f1efbf82a562
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-08 21:29:15 +0200

    fixed up c tests

--- 005/c/test.cpp
+++ 005/c/test.cpp
@@ -1,13 +1,5 @@
-#include <stdio.h>
-#include <gtest/gtest.h>
-
-#define TESTING
+#include "testmain.cpp"
 #include "solution.c"
-
-GTEST_API_ int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
 
 TEST(problem, easy) {
     EXPECT_EQ(2520, solve(10));
