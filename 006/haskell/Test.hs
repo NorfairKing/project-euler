35d3090867d1ff2ee1420769ffaa9370c7c99bbc
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-08 22:18:56 +0200

    better haskell tests

---
+++ 006/haskell/Test.hs
@@ -0,0 +1,2 @@
+test_easy = assertEqual (solve 10) 2640
+test_correct = assertEqual (solve 100 ) 25164150
--- 008/haskell/Test.hs
+++ 008/haskell/Test.hs
@@ -1,13 +1,2 @@
-{-# LANGUAGE CPP #-}
-{-# OPTIONS_GHC -F -pgmF htfpp #-}
-module Test where
-
-import           Test.Framework
-
-#include "solution.hs"
-
 test_easy = assertEqual (solve 4) 5832
 test_correct = assertEqual (solve 13) 23514624000
-
-test = htfMain htf_thisModulesTests
-
