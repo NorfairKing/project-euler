f111ad1b740b1d41e26dea73131cd33bf8d1a98b
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-09 00:22:03 +0200

    problem 6 and explanation

---
+++ 006/haskell/naive.hs
@@ -0,0 +1,13 @@
+solve :: Int -> Int
+solve p = squareOfSum p - sumOfSquares p
+
+-- The sum of squares of the first n integers
+sumOfSquares :: Int -> Int
+sumOfSquares n = sum $ map (^2) [1..n]
+
+-- The square of the sum of the first n integers
+squareOfSum :: Int -> Int
+squareOfSum n = (sum [1..n])^2
+
+main :: IO ()
+main = print $ solve 100
--- 006/haskell/solution.hs
+++ 006/haskell/solution.hs
@@ -1,13 +1,5 @@
--- The sum of squares of the first n integers
-sumOfSquares :: Int -> Int
-sumOfSquares n = sum $ map (^2) [1..n]
-
--- The square of the sum of the first n integers
-squareOfSum :: Int -> Int
-squareOfSum n = (sum [1..n])^2
-
-solve :: Int -> Int
-solve p = squareOfSum p - sumOfSquares p
+solve :: Integer -> Integer
+solve p = ((p*(p+1) `div` 2)^2) - ((p*(p+1)*(2*p+1)) `div` 6)
 
 main :: IO ()
-main = print $ solve 100
+main = print $ solve 10
