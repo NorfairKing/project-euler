7aa2ecf9468346933f591e8a9ca15aba5928c2f4
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-09 22:49:52 +0200

    more writeup

--- 001/haskell/solution.hs
+++ 001/haskell/solution.hs
@@ -4,4 +4,4 @@ main :: IO ()
 main = print $ solve 1000
 
 solve :: Int -> Int
-solve p = sum . nub $ [3, 6 .. (p-1)] ++ [5, 10 .. (p-1)]
+solve p = sum $ [3, 6 .. (p-1)] ++ [5, 10 .. (p-1)] ++ [-15, -30 .. -(p-1)]
