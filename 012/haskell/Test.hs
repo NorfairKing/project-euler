1a869981a7f3bcf02dd1308ccde18c39be224250
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-09 20:15:29 +0200

    solution for problem 12

---
+++ 012/haskell/Test.hs
@@ -0,0 +1,2 @@
+test_easy = assertEqual (solve 5) 28
+test_correct = assertEqual (solve 500) 76576500
---
+++ 012/haskell/solution.hs
@@ -0,0 +1,31 @@
+import           Control.Monad
+import           Data.List
+
+{-
+Triangle numbers are defined recursively but they can be computed explicityly.
+1 + 2 + ... + n = (n*(n+1))/2
+This means the nth triangle number can be calculated in O(1) time instead of O(n) time.
+-}
+triangles :: [Integer]
+triangles = [(i*(i+1) `div` 2) | i <- [1..]]
+
+{-
+Divisors can be calucated in O(n) time by checking every number that is smaller.
+We only need to check every number until sqrt(n) for divisors.
+We get the other half by dividing.
+-}
+divisors :: Integral a => a -> [a]
+divisors x = firstHalf ++ secondHalf
+        where
+            firstHalf = 1: (filter ((==0) . rem x) [2 .. sq x])
+            secondHalf = reverse [ x `div` i | i <- firstHalf]
+            sq = floor . sqrt . fromIntegral
+
+nr_divisors :: Integer -> Int
+nr_divisors = length . divisors
+
+solve :: Int -> Integer
+solve n = head [ x | x <- triangles, nr_divisors x >= n]
+
+main :: IO ()
+main = print $ solve 500
