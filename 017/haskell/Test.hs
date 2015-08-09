946131c2bb0361fa5b93ae8799e1658161a5aa1e
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-09 20:19:07 +0200

    solution for 17 in haskell

---
+++ 017/haskell/Test.hs
@@ -0,0 +1,2 @@
+test_easy = assertEqual (solve 5) 19
+test_correct = assertEqual (solve 1000) 21124
---
+++ 017/haskell/solution.hs
@@ -0,0 +1,29 @@
+import           Data.List
+
+smalls :: [String]
+smalls = [ "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" ]
+
+tens :: [String]
+tens = [ "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety", "hundred" ]
+
+-- Convert a small integer to its name.
+-- This method nicely shows how language is overly complex when it comes to numbers.
+stringify :: Int -> String
+stringify n | n < 20 = smalls !! n
+stringify n | n >= 20 && n < 100 = (tens !! (firstDigit - 2)) ++ stringify secondDigit
+    where
+        firstDigit = n `div` 10
+        secondDigit = n `rem` 10
+stringify n | n >= 100 && n < 1000 = smalls !! firstDigit ++ "hundred" ++ sndPart
+    where
+        sndPart = if rest == 0 then "" else "and" ++ stringify rest
+        firstDigit = n `div` 100
+        rest = n `rem` 100
+stringify 1000 = "onethousand"
+stringify _ = undefined
+
+solve :: Int -> Int
+solve p = sum $ map (length . stringify) [1..p]
+
+main :: IO ()
+main = print $ solve 1000
