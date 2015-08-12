9466624d2eeefc2ec84ab9db5c7b3c4c5ac48095
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-13 00:36:08 +0200

    writeup for a library

--- lib/c/order.c
+++ lib/c/order.c
@@ -1,9 +1,5 @@
 #include "order.h"
 
-int max(int a, int b) {
-    return a > b ? a : b;
-}
+int max(int a, int b) { return a > b ? a : b; }
 
-int min(int a, int b) {
-    return a < b ? a : b;
-}
+int min(int a, int b) { return a < b ? a : b; }
--- lib/c/order.h
+++ lib/c/order.h
@@ -1,7 +1,14 @@
 #ifndef order_h__
 #define order_h__
 
+/*
+ * Returns the maximum of two given numbers.
+ */
 int max(int a, int b);
+
+/*
+ * Returns the minimum of two given numbers.
+ */
 int min(int a, int b);
 
 #endif
