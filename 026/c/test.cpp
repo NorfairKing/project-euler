cae1aef1de81f1f1ac2eff75872d3acc5d6fa477
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-16 23:50:48 +0200

    smaller tests!

--- 026/c/test.cpp
+++ 026/c/test.cpp
@@ -1,7 +1,2 @@
-TEST(problem, easy) {
-    EXPECT_EQ(7, solve(7));
-}
-
-TEST(problem, correct) {
-    EXPECT_EQ(983, solve(1000));
-}
+TEST(problem, easy)    { EXPECT_EQ(7, solve(7)); }
+TEST(problem, correct) { EXPECT_EQ(983, solve(1000)); }
