f404f2c118cb8559bfd7707ef9a19d5e295f5459
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-16 23:47:37 +0200

    solution to 26 in c

---
+++ 026/c/solution.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define EMPTY -1
+
+int solve(int p);
+int decimals(int x, int y);
+
+#ifndef TESTING
+int main () {
+    printf("%d\n", solve(1000));
+    return 0;
+}
+#endif
+
+int solve(int p) {
+  int num = 0;
+  int maxlen = 0;
+  int d, len, i, x;
+  for (d = p; d >= 1; d--) {
+    if (maxlen > d) { break; }
+    int rems[p]; // d-1 possible remainders
+
+    for (i = 0; i < p; i++) { // Initialise all to predictable constant that will not be used
+      rems[i] = EMPTY; 
+    }
+
+    x = 1; // Secundary school division
+    i = 0; // Next number only depends on the remainder
+    while (rems[x] == EMPTY && x != 0) { // as soon as we find it again we'll have the cycle.
+      rems[x] = i++;
+      x *= 10;
+      x %= d;
+    }
+    
+    len = i - rems[x];
+    if (len > maxlen) {
+      maxlen = len;
+      num = d;
+    }
+  }
+  return num;
+}
---
+++ 026/c/test.cpp
@@ -0,0 +1,7 @@
+TEST(problem, easy) {
+    EXPECT_EQ(7, solve(7));
+}
+
+TEST(problem, correct) {
+    EXPECT_EQ(983, solve(1000));
+}
