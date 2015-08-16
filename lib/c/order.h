3177bec7224779444bc0d35d6f5f3cd98fd9d99b
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-17 00:10:49 +0200

    cleaned up lib code and writeup

--- lib/c/order.h
+++ lib/c/order.h
@@ -1,14 +1,10 @@
 #ifndef order_h__
 #define order_h__
 
-/*
- * Returns the maximum of two given numbers.
- */
+/* Returns the maximum of two given numbers. */
 int max(int a, int b);
 
-/*
- * Returns the minimum of two given numbers.
- */
+/* Returns the minimum of two given numbers. */
 int min(int a, int b);
 
 #endif
