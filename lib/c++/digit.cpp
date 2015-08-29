b861559506d39b85a26cf08762c45b73d935df4f
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-30 00:09:31 +0200

    32 in c++

--- lib/c++/digit.cpp
+++ lib/c++/digit.cpp
@@ -11,18 +11,6 @@ int nr_of_digits(int p) {
 }
 
 /*
- * DEPRECATED
- * Gets the nth digit of a given number p.
- * This will be 0 if n is greater than the number of digits of p.
- * Note that digits are counted from _least significant_ to _most significant_
- * and are _zero indexed_. That might seem backwards.
- */
-int digit(int n, int p) {
-  if (n == 0) { return p % 10; }
-  return digit(n-1, p/10);
-}
-
-/*
  * Calculates the number represented by the first n digits in an array of digits.
  * Whether the numbers in the array actually represent digits is not checked.
  */
---
+++ lib/c++/digit.h
@@ -0,0 +1,9 @@
+#ifndef digit_h__
+#define digit_h__
+
+#include <cmath>
+
+int nr_of_digits(int n);
+int from_digits(int digit[], int n);
+
+#endif
