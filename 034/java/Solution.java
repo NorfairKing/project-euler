7bbe780fe2f0c3d12535cfea8979af983315c6f6
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-30 03:20:27 +0200

    fixed up mkirsche's solution to 34

--- 034/java/Solution.java
+++ 034/java/Solution.java
@@ -1,23 +1,37 @@
-import java.util.*;
 public class Solution {
-public static void main(String[] args)
-{
-	long res = 0;
-	for(int i = 10; i<10000000; i++) if(i == go(i)) res += i;
-	System.out.println(res);
-}
-static boolean[] sieve;
-static int[] facts = new int[]{1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
-static int go(int n)
-{
-	int res = 0;
-	while(n > 0)
-	{
-		res += facts[n%10];
-		n /= 10;
-	}
-	return res;
-}
-}
+  static int[] facts = new int[]{1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
+  public static void main(String[] args) {
+    System.out.println(solve());
+  }
 
+  static int solve() {
+    cacheFactorials(10);
+    int res = 0;
+    int count = 0;
+    int i = 3;
+    while (count < 2) {
+      if (i == go(i)) {
+        res += i;
+        count++;
+      }
+      i++;
+    }
+    return res;
+  }
 
+  static void cacheFactorials(int n) {
+    facts[0] = 1;
+    for (int i = 1; i < n; i++) {
+      facts[i] = facts[i-1] * i;
+    }
+  }
+
+  static int go(int n) {
+    int res = 0;
+    while(n > 0) {
+      res += facts[n%10];
+      n /= 10;
+    }
+    return res;
+  }
+}
