20563dd0b924ee1e92cf1855eca5f57999c54192
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-30 03:37:18 +0200

    fixed 9 in java from mkirsche

--- 009/java/Solution.java
+++ 009/java/Solution.java
@@ -1,19 +1,23 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Solution {
-public static void main(String[] args)
-{
-	for(int i = 1; i<1000; i++)
-		for(int j = 1; j<1000; j++)
-		{
-			int sq = i*i + j*j;
-			int c = (int)(.5 + Math.sqrt(sq));
-			if(Math.abs(c - Math.sqrt(sq)) < 1e-9 && i + j + c == 1000)
-			{
-				System.out.println(i*j*c);
-				return;
-			}
-		}
-}
-}
+  public static void main(String[] args)
+  {
+    Scanner input = new Scanner(System.in);
+    int n = input.nextInt();
+    System.out.println(solve(n));
+  }
 
-
+  static int solve(int n) {
+    for (int i = 1; i < n; i++) {
+      for (int j = 1; j < n; j++) {
+        int sq = i*i + j*j;
+        int c = (int)(.5 + Math.sqrt(sq));
+        if (Math.abs(c - Math.sqrt(sq)) < 1e-9 && i + j + c == n) {
+          return i*j*c;
+        }
+      }
+    }
+    return -1;
+  }
+}
