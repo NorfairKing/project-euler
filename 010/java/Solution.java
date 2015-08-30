7fcb2bf9ad933dfec04d356ba6151653782a4627
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-30 03:46:08 +0200

    fixed mkirsche's 10

--- 010/java/Solution.java
+++ 010/java/Solution.java
@@ -1,23 +1,29 @@
-import java.util.*;
-public class Solution {
-public static void main(String[] args)
-{
-	Scanner input = new Scanner(System.in);
-	int n = input.nextInt();
-	boolean[] sieve = new boolean[n+1];
-	Arrays.fill(sieve,  true);
-	sieve[0] = sieve[1] = false;
-	long res = 0;
-	for(int i = 2; i<=n; i++)
-	{
-		if(sieve[i])
-		{
-			res += i;
-			for(long j = (long)i*i; j <= n; j += i) sieve[(int)j] = false;
-		}
-	}
-	System.out.println(res);
-}
-}
+import java.util.Scanner;
+import java.util.Arrays;
 
+public class Solution {
+  public static void main(String[] args)
+  {
+    Scanner input = new Scanner(System.in);
+    int n = input.nextInt();
+    System.out.println(solve(n));
+  }
+
+  static long solve(int n) {
+    boolean[] sieve = new boolean[n+1];
+    Arrays.fill(sieve,  true);
+    long res = 0;
+    sieve[0] = sieve[1] = false;
+    for(int i = 2; i<=n; i++)
+    {
+      if(sieve[i])
+      {
+        res += i;
+        for(long j = (long)i*i; j <= n; j += i)
+          sieve[(int)j] = false;
+      }
+    }
+    return res;
+  }
+}
 
