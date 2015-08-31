342b2a43b41aefb49e7a06f63c5f09e9ccc0aab6
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-31 22:52:53 +0200

    41 in hand-wavy haskell

--- lib/haskell/Primes.hs
+++ lib/haskell/Primes.hs
@@ -4,3 +4,6 @@ primes :: [Integer]
 primes = sieve [2..]
   where sieve (p:xs) = p : sieve [ x | x <- xs, x `rem` p /= 0]
 
+isPrime :: Integer -> Bool
+isPrime n = all (\p -> n `rem` p /= 0) [2.. sq n]
+  where sq = ceiling . sqrt . fromInteger
