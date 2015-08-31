db54085920cc8de7deb82a424c0e0c2e4f207f62
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-08-31 20:31:11 +0200

    fromDigits

--- lib/haskell/Digit.hs
+++ lib/haskell/Digit.hs
@@ -7,3 +7,10 @@ digits x = (x `rem` 10):(digits $ x `div` 10)
 nrDigits :: Integer -> Integer
 nrDigits x | x < 10 = 1
 nrDigits x = 1 + (nrDigits $ x `div` 10)
+
+fromDigits :: [Integer] -> Integer
+fromDigits = go 1 . reverse
+  where
+    go :: Integer -> [Integer] -> Integer
+    go _ [] =  0
+    go b (d:ds) = b * d + go (10*b) ds
