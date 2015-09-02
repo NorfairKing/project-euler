06aab4997fc9bcbd3031cb638043c6c3dc71dc2b
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-09-02 20:54:01 +0200

    another algorithms documented

--- lib/c/series.c
+++ lib/c/series.c
@@ -6,6 +6,7 @@ int sum_naturals(int n) {
   return (n * (n + 1)) / 2;
 }
 
+// This is being used in series.tex
 double sum_geometric(double a, double n, double r){
   return a * (1 - pow(r, n)) / (1 - r);
 }
