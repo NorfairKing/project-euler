6ee5d094d9c4bd99bc00718fd0c0046db1cbc912
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-09-06 01:32:29 +0200

    fixed scala libaries

--- lib/scala/projecteuler/Multiple.scala
+++ lib/scala/projecteuler/Multiple.scala
@@ -1,8 +1,10 @@
 package projecteuler
 
+import projecteuler.Series.sumNaturals
+
 object Multiple {
   def multiplesUnder(p: Int, n: Int): Int = {
     val pdn = p/n - (if(p % n == 0) 1 else 0)
-    (n * pdn * (pdn + 1)) / 2
+    return n * sumNaturals(pdn)
   }
 }
