66f0f8ff710f83b2045d34b6eec40d01feed3ab4
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-09-08 18:37:09 +0200

    solve 7 in c++

---
+++ 007/c++/solution.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include "prime_sieve.h"
+
+int solve(int p);
+
+#ifndef TESTING
+int main () {
+  int p;
+  std::cin >> p;
+  std::cout << solve(p) << std::endl;
+}
+#endif
+
+int solve(int p) {
+  prime_sieve is_prime;
+
+  int j = 0; // j is the
+  int i = 0; // ith prime
+  while (i < p) {
+    j++;
+    if (is_prime[j]){
+      i++;
+    }
+  }
+  return j;
+}
