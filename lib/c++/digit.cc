0592644275ec416eae94cfc007044f759f3b6019
Tom Sydney Kerckhove <syd.kerckhove@student.kuleuven.be>
2015-09-20 11:42:02 +0200

    solution and writeup for 4 in c++

--- lib/c++/digit.cc
+++ lib/c++/digit.cc
@@ -3,33 +3,27 @@
 #include "digit.h"
 #include "base.h"
 
-/*
- * Gets the number of digits in a given number.
- */
+// Calculates the number of digits of a given number.
 int nr_of_digits(int p) {
   if (p < BASE) { return 1; }
   return nr_of_digits(p / BASE) + 1;
 }
 
-/*
- * Calculates the number represented by the first n digits in an array of digits.
- * Whether the numbers in the array actually represent digits is not checked.
- */
-int from_digits(int digit[], int n) {
-  int j; int num = 0;
-  for (j = 0; j < n; j++){
+// Calculates a number from its digital representation.
+int from_digits(std::vector<int> digit) {
+  int n = digit.size();
+  int num = 0;
+  for (int j = 0; j < n; j++){
     num += digit[j] * pow(BASE, n - 1 - j);
   }
   return num;
 }
 
-/*
- * Gets a vector of the digits of n in reverse order.
- */
+// Calculates a vector of the digits of a given number
 std::vector<int> digits(int n) {
-  std::vector<int> digits;
-  while (n > 0) {
-    digits.push_back(n % BASE);
+  std::vector<int> digits(nr_of_digits(n));
+  for (int i = n-1; i >= 0; --i) {
+    digits[i] = n % BASE;
     n /= BASE;
   }
   return digits;
--- lib/c++/digit.h
+++ lib/c++/digit.h
@@ -5,7 +5,7 @@
 #include <vector>
 
 int nr_of_digits(int n);
-int from_digits(int digit[], int n);
+int from_digits(std::vector<int> digit);
 std::vector<int> digits(int n);
 
 #endif
