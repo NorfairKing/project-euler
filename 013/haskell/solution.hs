3cf1dff9873268b9e5e23c6bbcb66d01c071efe7
Tom Sydney Kerckhove <syd.kerckhove@gmail.com>
2017-04-22 23:03:54 +0200

    Updated problem 13 to have a more verbose test case

--- 013/haskell/solution.hs
+++ 013/haskell/solution.hs
@@ -1,4 +1,4 @@
-import           Control.Monad (replicateM)
+import Control.Monad (replicateM)
 
 solve :: [Integer] -> Integer
 solve = read . take 10 . show . sum
@@ -6,5 +6,7 @@ solve = read . take 10 . show . sum
 main :: IO ()
 main = do
     _ <- getLine
-    ints <- replicateM 100 readLn
+    _ <- getLine
+    n <- readLn
+    ints <- replicateM n readLn
     print $ solve ints
--- 013/java/Solution.java
+++ 013/java/Solution.java
@@ -5,12 +5,14 @@ public class Solution {
   public static void main(String[] args)
   {
     Scanner input = new Scanner(System.in);
+    int p = input.nextInt();
+    input.nextInt();
     BigInteger res = new BigInteger("0");
     int n = input.nextInt();
     for (int i = 0; i < n; i++) {
       res = res.add(new BigInteger(input.next()));
     }
-    System.out.println(res.toString().substring(0, 10));
+    System.out.println(res.toString().substring(0, p));
   }
 }
 
